54fa6acb6265d55b06c874adff82f1d729722ee2
Ivan Novozhilov <inovozhilov@protonmail.com>
2024-04-02 03:37:34 +0300

    controller advice; check users

---
+++ src/main/java/com/tasktrace/controller/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.tasktrace.controller;
+
+import com.tasktrace.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {EntityNotFoundException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Сущность не найдена";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
--- src/main/java/com/tasktrace/service/TaskServiceImpl.java
+++ src/main/java/com/tasktrace/service/TaskServiceImpl.java
@@ -4,6 +4,7 @@ import com.tasktrace.dto.CreateTaskDto;
 import com.tasktrace.dto.TaskDto;
 import com.tasktrace.dto.UpdateTaskDto;
 import com.tasktrace.exception.EntityNotFoundException;
+import com.tasktrace.feign.TasktraceUsers;
 import com.tasktrace.mapper.TaskMapper;
 import com.tasktrace.model.Label;
 import com.tasktrace.model.Priority;
@@ -13,6 +14,7 @@ import com.tasktrace.repository.LabelRepository;
 import com.tasktrace.repository.PriorityRepository;
 import com.tasktrace.repository.StatusRepository;
 import com.tasktrace.repository.TaskRepository;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -33,12 +35,16 @@ public class TaskServiceImpl implements TaskService {
 
     private final StatusRepository statusRepository;
 
+    private final TasktraceUsers tasktraceUsers;
+
     public TaskServiceImpl(TaskRepository taskRepository, PriorityRepository priorityRepository,
-                           LabelRepository labelRepository, StatusRepository statusRepository) {
+                           LabelRepository labelRepository, StatusRepository statusRepository,
+                           TasktraceUsers tasktraceUsers) {
         this.taskRepository = taskRepository;
         this.priorityRepository = priorityRepository;
         this.labelRepository = labelRepository;
         this.statusRepository = statusRepository;
+        this.tasktraceUsers = tasktraceUsers;
     }
 
     @Transactional(readOnly = true)
@@ -85,6 +91,17 @@ public class TaskServiceImpl implements TaskService {
 
     private TaskDto save(long id, String title, String reporter, String performer, String text, long priorityId,
                          List<Long> labelIds, long statusId) {
+        List<UserRepresentation> reporterUR = tasktraceUsers.searchByUsername(reporter);
+        List<UserRepresentation> performerUR = tasktraceUsers.searchByUsername(performer);
+
+        if (reporterUR.isEmpty()) {
+            throw new EntityNotFoundException("Reporter user with name %s not found".formatted(reporter));
+        }
+
+        if (performerUR.isEmpty()) {
+            throw new EntityNotFoundException("Performer user with name %s not found".formatted(performer));
+        }
+
         Priority priority = priorityRepository.findById(priorityId)
                 .orElseThrow(() -> new EntityNotFoundException("Priority with id %d not found".formatted(id)));
         List<Label> labels = labelRepository.findAllById(labelIds);
