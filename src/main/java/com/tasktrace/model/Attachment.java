db5f5e702ec083097f92d2834fe357b453249ab6
Ivan Novozhilov <nip1992@mail.ru>
2024-08-23 21:13:06 -0300

    Refactor imports to remove unused `jakarta.persistence.CascadeType`.

--- src/main/java/com/tasktrace/model/Attachment.java
+++ src/main/java/com/tasktrace/model/Attachment.java
@@ -1,6 +1,5 @@
 package com.tasktrace.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
